91e8e6fb740b3ab5bdbc91a94047567bd101a21f
Victor Anyadioha <talk2okwundu@gmail.com>
2022-10-21 03:34:19 +0100

    Basic Project structure created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+const App = () => {
+  const [floatingButton, setFloatingButton] = useState()
+  return (
+    <div className="App">
+      <header className="hero-section">
+        <section className="user-info">
+          <div className="img-container"><img src="" alt="" /></div>
+          <h1 className="seller-name"></h1>
+        </section>
+      </header>
+      <ul className="affiliate-links">
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+        <li className="affiliate-link">
+          <button>
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+          </button>
+        </li>
+      </ul>
+      <div className="social-icons-container"></div>
+      <footer><div></div></footer>
+    </div>
+  )
 }
 
 export default App;
