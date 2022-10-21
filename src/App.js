19df5f864bc8bf09b3be3024c2686d6fddb9f546
Victor Anyadioha <talk2okwundu@gmail.com>
2022-10-21 15:38:26 +0100

    Styling Done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,102 @@
 import { useState } from "react";
-import { FaSlack, FaTwitter, FaGithub } from 'react-icons/fa';
+import { FaSlack, FaTwitter, FaGithub } from "react-icons/fa";
 import "./App.css";
-import profilePic from './assets/person2.jpg';
-import i4g from './assets/i4g.jpg'
+import profilePic from "./assets/person2.jpg";
+import i4g from "./assets/i4g.jpg";
 
 const App = () => {
-  const [floatingButton, setFloatingButton] = useState()
+  // const [floatingButton, setFloatingButton] = useState()
   return (
     <div className="App">
       <header className="hero-section">
         <section className="user-info">
-          <div className="img-container"><img src={profilePic} alt="Profile Picture" id="profile__img" /></div>
+          <div className="img-container">
+            <img src={profilePic} alt="Profile Picture" id="profile__img" />
+          </div>
           <h1 className="seller-name">Chiamaka Opara</h1>
-          <div><a href="https://twitter.com/thgr8khallie" target="_blank" rel="noopener noreferrer" id="twitter"><FaTwitter /> @thegr8khallie</a></div>
-          <div><a href="https://" target="_blank" rel="noopener noreferrer" ><FaSlack /> @talk2okwundu</a></div>
         </section>
       </header>
       <ul className="affiliate-links">
         <li className="affiliate-link">
-          <button id="btn__zuri">
-            <a href="https://training.zuri.team" target="_blank" rel="noopener noreferrer"></a>
-          </button>
+          <a
+            href="https://twitter.com/thegr8khallie"
+            target="_blank"
+            rel="noopener noreferrer"
+            id="twitter"
+          >
+            <button id="btn__zuri">Twitter Link</button>
+          </a>
+        </li>
+        <li className="affiliate-link" id="slack-link">
+          <a href="https://" target="_blank" rel="noopener noreferrer">
+            <button id="btn__zuri">Slack Link</button>
+          </a>
         </li>
         <li className="affiliate-link">
-          <button id='books'>
-            <a href="http://books.zuri.team" target="_blank" rel="noopener noreferrer"></a>
-          </button>
+          <a
+            href="https://training.zuri.team"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button id="btn__zuri">Zuri Team</button>
+          </a>
         </li>
         <li className="affiliate-link">
-          <button id="book__python">
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=talk2okwundu" target="_blank" rel="noopener noreferrer"></a>
-          </button>
+          <a
+            href="http://books.zuri.team"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button id="books">Zuri Books</button>
+          </a>
         </li>
         <li className="affiliate-link">
-          <button id="pitch">
-            <a href="https://background.zuri.team" target="_blank" rel="noopener noreferrer"></a>
-          </button>
+          <a
+            href="https://books.zuri.team/python-for-beginners?ref_id=talk2okwundu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button id="book__python">Python Books</button>
+          </a>
         </li>
         <li className="affiliate-link">
-          <button id="book__design">
-            <a href="https://books.zuri.team/design-rules" target="_blank" rel="noopener noreferrer"></a>
-          </button>
+          <a
+            href="https://background.zuri.team"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button id="pitch">Background checks for coders</button>
+          </a>
+        </li>
+        <li className="affiliate-link">
+          <a
+            href="https://books.zuri.team/design-rules"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button id="book__design">Design books</button>
+          </a>
         </li>
       </ul>
-      <div className="social-icons-container"><FaSlack /> <FaGithub /></div>
+      <div className="social-icons-container">
+        <span>
+          <FaSlack />
+        </span>
+        <span>
+          <FaGithub />
+        </span>
+      </div>
       <footer>
-        <div className="branding">Zuri<span />Internship</div>
-        <div className="branding">HNG Internship 9 frontend task</div>
-        <div className="branding"><img src={i4g} alt="Ingressive for good" /></div>
+        <div id="zuri-icon">
+          Zuri <span /> Internship
+        </div>
+        <div id="hng-icon">HNG Internship 9 frontend task</div>
+        <div id="i4g-icon">
+          <img src={i4g} alt="Ingressive for good" />
+        </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default App;
