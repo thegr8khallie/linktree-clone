1233a34c5fc2e6b1137510e5dde934c05b84a318
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-04 15:51:23 +0100

    Style: Done with all styles for default state.

--- src/screens/Contacts.jsx
+++ src/screens/Contacts.jsx
@@ -5,17 +5,21 @@ const Contacts = () => {
             <p>Hi there, contact me to ask me about anything you have in mind.</p>
             <form className="contacts-form">
                 <div className="name-fields">
-                    <label htmlFor="first_name">First name</label>
-                    <input type="text" id="first_name" placeholder="Enter your first name" />
-                    <label htmlFor="last_name">Last name</label>
-                    <input type="text" id="last_name" placeholder="Enter your last name" />
+                    <div className="first-name-field">
+                        <label htmlFor="first_name">First name</label>
+                        <input type="text" id="first_name" placeholder="Enter your first name" />
+                    </div>
+                    <div className="last-name-field">
+                        <label htmlFor="last_name">Last name</label>
+                        <input type="text" id="last_name" placeholder="Enter your last name" />
+                    </div>
                 </div>
                 <label htmlFor="email">Email</label>
                 <input type="text" id="email" placeholder="yourname@email.com" />
                 <label htmlFor="message">Message</label>
                 <textarea id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." />
-                <label htmlFor="agreement">You agree to providing your data to Chiamaka Opara who may contact you.</label>
                 <input type="checkbox" id="agreement" />
+                <label htmlFor="agreement">You agree to providing your data to Chiamaka Opara who may contact you.</label>
                 <button type="submit" id="btn__submit">Send Message</button>
             </form>
         </section>
