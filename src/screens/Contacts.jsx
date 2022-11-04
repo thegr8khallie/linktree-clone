50f93b1eae0683f9ac9d33b50e35d6f5f3b3457a
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-04 18:55:50 +0100

    Feat: Set up state variables for all input fields and linked them to the appropriate input fields.

--- src/screens/Contacts.jsx
+++ src/screens/Contacts.jsx
@@ -1,25 +1,36 @@
+import { useState } from "react"
+
 const Contacts = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+
+    const sendMessageHandler = (e) => {
+        e.preventDefault();
+    }
     return (
         <section className="form-container">
             <h1 className="form-header">Contact Me</h1>
             <p>Hi there, contact me to ask me about anything you have in mind.</p>
-            <form className="contacts-form">
+            <form className="contacts-form" onSubmit={sendMessageHandler}>
                 <div className="name-fields">
                     <div className="first-name-field">
                         <label htmlFor="first_name">First name</label>
-                        <input type="text" id="first_name" placeholder="Enter your first name" />
+                        <input type="text" id="first_name" placeholder="Enter your first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                     </div>
                     <div className="last-name-field">
                         <label htmlFor="last_name">Last name</label>
-                        <input type="text" id="last_name" placeholder="Enter your last name" />
+                        <input type="text" id="last_name" placeholder="Enter your last name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
                     </div>
                 </div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" placeholder="yourname@email.com" />
+                <input type="text" id="email" placeholder="yourname@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="message">Message</label>
-                <textarea id="message" cols="30" rows="6" placeholder="Send me a message and I'll reply you as soon as possible..." />
+                <textarea id="message" cols="30" rows="6" placeholder="Send me a message and I'll reply you as soon as possible..." value={message} onChange={(e) => setMessage(e.target.value)} />
                 <div className="agreement">
-                    <input type="checkbox" id="agreement" />
+                    <input type="checkbox" id="agreement" checked={checkbox} value={checkbox} onChange={(e) => setCheckbox(e.currentTarget.checked)} />
                     <label htmlFor="agreement">You agree to providing your data to Chiamaka Opara who may contact you.</label>
                 </div>
                 <button type="submit" id="btn__submit">Send Message</button>
