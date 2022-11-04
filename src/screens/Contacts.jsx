4dc491e5d56d9daed529b5643399d98130d78153
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-04 05:06:06 +0100

    Feat: Added placeholders to input fields

--- src/screens/Contacts.jsx
+++ src/screens/Contacts.jsx
@@ -4,14 +4,16 @@ const Contacts = () => {
             <h1 className="form-header">Contact Me</h1>
             <p>Hi there, contact me to ask me about anything you have in mind.</p>
             <form className="contacts-form">
-                <label htmlFor="first_name">First name</label>
-                <input type="text" id="first_name" />
-                <label htmlFor="last_name">Last name</label>
-                <input type="text" id="last_name" />
+                <div className="name-fields">
+                    <label htmlFor="first_name">First name</label>
+                    <input type="text" id="first_name" placeholder="Enter your first name" />
+                    <label htmlFor="last_name">Last name</label>
+                    <input type="text" id="last_name" placeholder="Enter your last name" />
+                </div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input type="text" id="email" placeholder="yourname@email.com" />
                 <label htmlFor="message">Message</label>
-                <textarea id="message" cols="30" rows="10"></textarea>
+                <textarea id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." />
                 <label htmlFor="agreement">You agree to providing your data to Chiamaka Opara who may contact you.</label>
                 <input type="checkbox" id="agreement" />
                 <button type="submit" id="btn__submit">Send Message</button>
